92d51a17dff47ecdedb282239990992a57fc7d48
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-18 13:17:51 +0400

    replace sorted func with order_by.

--- blog/views.py
+++ blog/views.py
@@ -40,8 +40,7 @@ def index(request):
     most_fresh_posts = list(fresh_posts)[-5:]
 
     tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = tags.annotate(tags_count=Count('posts')).order_by('-tags_count')[:5]
 
     context = {
         'most_popular_posts': [
